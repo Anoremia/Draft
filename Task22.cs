930bca27bcf40947785a4ba447b279df55fd581b
Polina Kozlova <158488988+Anoremia@users.noreply.github.com>
2024-02-19 23:46:17 +0400

    Add files via upload
    Создайте и реализуйте метод, который будет принимать число по ссылке (ref) и менять его знак на противоположный. Метод должен принимать один аргумент и не возвращать ничего.

---
+++ Task22.cs
@@ -0,0 +1,22 @@
+﻿namespace Task22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 7;
+
+            Console.WriteLine("Исходное значение числа: " + number);
+
+            ChangeSign(ref number);
+
+            Console.WriteLine("Число после изменения знака: " + number);
+        }
+
+        static void ChangeSign(ref int num)
+        {
+            num = -num;
+        }
+    }
+    
+}
