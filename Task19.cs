4ceca2a7824fcccb98da1eee5f021bac1aa3a9bf
Polina Kozlova <158488988+Anoremia@users.noreply.github.com>
2024-02-19 23:45:17 +0400

    Add files via upload
    Создайте и реализуйте метод, который вычисляет сумму двух чисел и возвращает их сумму. Вызовите метод в методе Main и выведите результат на экран. Два числе передайте в метод как вам угодно. Метод, который вы напишите, не должен работать с консолью.

---
+++ Task19.cs
@@ -0,0 +1,18 @@
+﻿namespace Task19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 150;
+            int b = 150;
+            Sum(a,b);
+        }
+
+        public static int Sum(int x, int y)
+        {
+            return x + y;   
+        }
+    }
+
+}
