76919bc14109c434a1cba1a14419086008d811cc
Polina Kozlova <158488988+Anoremia@users.noreply.github.com>
2024-02-19 23:46:55 +0400

    Add files via upload
    Создайте и реализуйте метод, который будет принимать массив на вход убирать из него отрицательные числа и возвращать новый изменённый массив. Через out параметр возвращать количество удалённых символов.

---
+++ Task24.cs
@@ -0,0 +1,55 @@
+﻿namespace Task24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { -1, -2, 3, -4, 5, -6 };
+            int removedCount;
+
+            int[] result = RemoveNegativeNumbers(numbers, out removedCount);
+
+            Console.WriteLine("Исходный массив:");
+            PrintArray(numbers);
+
+            Console.WriteLine("\nМассив после удаления отрицательных чисел:");
+            PrintArray(result);
+
+            Console.WriteLine($"\nКоличество удаленных отрицательных чисел: {removedCount}");
+        }
+
+        static int[] RemoveNegativeNumbers(int[] array, out int removedCount)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < 0)
+                {
+                    count++;
+                }
+            }
+
+            int[] result = new int[array.Length - count];
+            int index = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] >= 0)
+                {
+                    result[index] = array[i];
+                    index++;
+                }
+            }
+
+            removedCount = count;
+            return result;
+        }
+
+        static void PrintArray(int[] array)
+        {
+            foreach (var num in array)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
