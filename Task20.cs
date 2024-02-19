af9648960e07a4104b9878fe91cee12bbb9a5ed8
Polina Kozlova <158488988+Anoremia@users.noreply.github.com>
2024-02-19 23:45:38 +0400

    Add files via upload
    Создайте и реализуйте метод, который принимает на вход размер массива n и возвращает пустой массив указанного размера. Вызовите метод в методе Main и заполните его полученный массив. Содержимое массива выведите на экран.

---
+++ Task20.cs
@@ -0,0 +1,31 @@
+﻿namespace Task20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5; // Размер массива
+
+            int[] emptyArray = CreateEmptyArray(n);
+
+            Console.WriteLine("Содержимое массива:");
+
+            foreach (int num in emptyArray)
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+        static int[] CreateEmptyArray(int n)
+        {
+            int[] newArray = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                newArray[i] = i + 1; // Заполнение массива значениями от 1 до n
+            }
+
+            return newArray;
+        }
+    }
+}
