e56647a22a7d9514414a14bb581359ad2552ace1
Polina Kozlova <158488988+Anoremia@users.noreply.github.com>
2024-02-19 23:46:40 +0400

    Add files via upload
    Создайте и реализуйте метод, который будет принимать два числа и менять их значения местами. Вызовите метод в Main.

---
+++ Task23.cs
@@ -0,0 +1,25 @@
+﻿namespace Task23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 20;
+
+            Console.WriteLine($"Исходные значения: num1 = {num1}, num2 = {num2}");
+
+            SwapNumbers(ref num1, ref num2);
+
+            Console.WriteLine($"Числа после замены: num1 = {num1}, num2 = {num2}");
+        }
+
+        static void SwapNumbers(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+   
+}
