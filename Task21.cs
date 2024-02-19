3a97039aa02f503be3e3bf529be4f4562fda58ed
Polina Kozlova <158488988+Anoremia@users.noreply.github.com>
2024-02-19 23:45:55 +0400

    Add files via upload
    Создайте и реализуйте метод, который принимает на вход массив и инвертирует его. Вызовите его в методе Main. Исходный массив задайте сами. Инвертированный массив выведите на экран консоли.

---
+++ Task21.cs
@@ -0,0 +1,38 @@
+﻿namespace Task21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] originalArray = { 1, 2, 3, 4, 5 };
+
+            int[] reversedArray = ReverseArray(originalArray);
+
+            Console.WriteLine("Исходный массив:");
+            PrintArray(originalArray);
+
+            Console.WriteLine("\nИнвертированный массив:");
+            PrintArray(reversedArray);
+        }
+
+        static int[] ReverseArray(int[] array)
+        {
+            int[] reversed = new int[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                reversed[i] = array[array.Length - 1 - i];
+            }
+
+            return reversed;
+        }
+
+        static void PrintArray(int[] array)
+        {
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
